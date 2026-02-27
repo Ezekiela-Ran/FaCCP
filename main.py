8269623cc0fc46bc6ed108700801452ef723afca
Ezekiela <randriamanalinanambinintsoa@gmail.com>
2026-02-27 17:49:09 +0300

    Premier commit

---
+++ main.py
@@ -0,0 +1,30 @@
+"""
+    This is the main entry point of the application
+"""
+import sys
+from PySide6 import QtWidgets
+
+class Window(QtWidgets.QWidget):
+    
+    """
+        This is the main window of the application.
+        It sets up the window title and its geometry to match the screen size.
+    """
+    def __init__(self):
+        super().__init__()
+        
+        height = self.screen().size().height()
+        width = self.screen().size().width()
+        
+        self.setWindowTitle("Acssqda")
+        self.setGeometry(0, 0, width, height)
+        
+        
+if __name__ == "__main__":
+    
+    app = QtWidgets.QApplication([])
+
+    win = Window()
+    win.show()
+
+    sys.exit(app.exec())

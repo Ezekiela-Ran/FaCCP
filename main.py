bf647299224002d93de8d7f39db2cd7054b19ab2
Ezekiela <randriamanalinanambinintsoa@gmail.com>
2026-02-28 10:57:17 +0300

    ajout de la barre de menu

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 """
 import sys
 from PySide6 import QtWidgets
+from views.components.menu_bar import MenuBar
 
 class Window(QtWidgets.QWidget):
     
@@ -25,6 +26,7 @@ if __name__ == "__main__":
     app = QtWidgets.QApplication([])
 
     win = Window()
+    MenuBar(win)
     win.show()
-
+    
     sys.exit(app.exec())
